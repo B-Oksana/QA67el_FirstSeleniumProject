516e9da15ee95904e36f51f6177c803197c99bb4
B-Oksana <my_liberty2@ukr.net>
2026-03-12 21:59:35 +0100

    Add 1

---
+++ src/test/java/com/ait/tests/FirstSeleniumTest.java
@@ -0,0 +1,36 @@
+package com.ait.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FirstSeleniumTest {
+
+    WebDriver driver;
+
+    // before test
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com/");                 // без истории
+       // driver.navigate().to("https://www.google.com/");     // с историей
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+    }
+
+    // test
+    @Test
+    public void openBrowser() {
+        System.out.println("Google opened!!!");
+    }
+
+    // after test
+    @AfterMethod (enabled = false)
+    public  void  tearDown() {
+        driver.quit();        // закрывает все вкладки и браузер
+       // driver.close();     // закрывает только одну вкладку
+    }
+}
